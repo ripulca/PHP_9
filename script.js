c447c8f7e9021fa00dfb510be634d5ed4f40c187
ripulca <68591785+ripulca@users.noreply.github.com>
2022-05-24 12:24:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ $('.submit_btn').click(function(e){
         },
         success(data) {
             if(data.status) {
-                $('.feedback_form').removeClass('close').addClass(' open');
-                $('.form').addClass(' close');
+                $('.feedback_form').removeClass('close').addClass(' open'); //показываем форму ответа
+                $('.form').addClass(' close');      //скрываем начальную форму
                 $('.feedback_form_msg').text("");
                 // $('.feedback_form_msg').append(data.datas);
                 // $('.feedback_form_msg').append("<br>");
                 data.datas.forEach(msg => {
-                    $('.feedback_form_msg').addClass(' open').append(msg);
+                    $('.feedback_form_msg').addClass(' open').append(msg);  //добавляем данные в форму
                     $('.feedback_form_msg').append("<br>");
                 });
             }
             else{
-                $('.error_msg').text("");
+                $('.error_msg').text("");   //если есть ошибки
                 data.errors.forEach(error => {
-                    $('.error_msg').addClass(' open').append(error);
+                    $('.error_msg').addClass(' open').append(error);    //добавить текст ошибки
                     $('.error_msg').append("<br>");
                 });
             }
