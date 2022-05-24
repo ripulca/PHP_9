aa66a28f635337f5e43fbe5000de2662c9db106d
ripulca <68591785+ripulca@users.noreply.github.com>
2022-05-24 12:09:33 +0300

    Задание с API

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- <link rel="stylesheet" href="style.css"> -->
 </head>
 <body>
-    <form name='search_form' class="form" method='POST'>
+    <form name='search_form' class="form" method='POST'>    //форма поиска
           <label for="adress">Адрес:</label><br>
           <input type="text" id="adress" name="adress" required="required"><br>
           <p class="error_msg"></p>
@@ -15,10 +15,10 @@
     </form>
     
     <div class="feedback_form close" id="feedback_form">
-        <div class="feedback_form_msg" id="feedback_form_msg">
+        <div class="feedback_form_msg" id="feedback_form_msg">  //поле для вывода инфы
         </div>
     </div>
 </body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>    //скрипт яндекса
 <script src="script.js"></script>
 </html>
